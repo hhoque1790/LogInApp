68111d7ed13caaf6c71c67dcefcb8066abf71472
Hassan Hoque <132135307+hhoque1790@users.noreply.github.com>
2023-09-05 09:46:07 +0100

    Update app.js- changed callbackURL for github strategy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/github/callback",
+      callbackURL: `http://localhost:${PORT}/auth/github/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
       done(null, profile);
